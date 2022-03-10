d351444ee7b41e13f937d7237311ecdc9c586f9b
Subhash_vikram <vsubhash3111@gmail.com>
2022-03-11 00:13:28 +0530

    Created Login API

--- msgapp/database.py
+++ msgapp/database.py
@@ -1,27 +1,44 @@
 from pymongo import MongoClient
+import configparser
 
-CONNECTION_STRING = "mongodb+srv://Subhash3111:subhash1031@msgapp.gyttk.mongodb.net/msgpp?retryWrites=true&w=majority"
+# Parsing Config File
+PARSER = configparser.ConfigParser()
+PARSER.read("E:\\Messnging_App\\Config\\Config.config")
+CONNECTION_STRING = PARSER.get('Database', 'conn_string')
+DATABASE = PARSER.get('Database', 'database')
+CONNECTION_STRING = CONNECTION_STRING.replace('<username>', PARSER.get('Database', 'username'))
+CONNECTION_STRING = CONNECTION_STRING.replace('<password>', PARSER.get('Database', 'password'))
+CONNECTION_STRING = CONNECTION_STRING.replace('<database>', DATABASE)
 
 
 def connect():
     # Used to connect to Database
     database = MongoClient(CONNECTION_STRING)
-    return database['msgapp']
+    return database[DATABASE]
+
+
+def username_exists(col_user, username: str) -> bool:
+    # check username exists or not
+    user = col_user.count_documents({"Username": username})
+    if user:
+        return True
+    else:
+        return False
 
 
 def auth_user(username: str, password: str):
     # checks weather user exists or not
     db = connect()
     col_user = db['users']
-    user_details = col_user.find({"username": username, "password": password})
-    if user_details:
-        for item in user_details:
-            print(item)
+    if username_exists(col_user, username):
+        user_details = col_user.count_documents({"Username": username, "Password": password})
+        if user_details:
+            return {"Status": "Success", "Message": "Login Successful"}
+        else:
+            return {"Status": "Failed", "Message": "Password doesn't match."}
     else:
-        print("user doesn't exists...")
-
+        return {"Status": "Failed", "Message": "username doesn't exists..."}
 
 
 if __name__ == '__main__':
-    auth_user('Subhash', 'subh@sh1031')
-
+    auth_user('subhash', 'subh@sh1031')
--- msgapp/views.py
+++ msgapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from msgapp.database import auth_user
+
 
 # Create your views here.
 
@@ -9,5 +11,5 @@ class Login(APIView):
     def get(self, request):
         username = request.META.get('HTTP_USERNAME')
         password = request.META.get('HTTP_PASSWORD')
-
-        return JsonResponse({"Status": "Login Successfully"})
+        status = auth_user(username, password)
+        return JsonResponse(status)
